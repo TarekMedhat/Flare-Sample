81005b05390ebf6cacc9ec86341632182a9fe89d
Tarek Tolba <tarek.tolba@mobidevlabs.com>
2019-11-20 14:21:51 +0200

    flutter flare

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Container(
+//        decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/BG1.png'))),
+        child: FlareActor("assets/Activities.flr",
+            animation: "up down",
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
